c4dabe06b402e1a72ac86834b467845fae7a871b
xmppjingle <barata7@gmail.com>
2016-09-05 12:19:53 -0300

    Stop Message

--- mod_grafite/src/mod_grafite.erl
+++ mod_grafite/src/mod_grafite.erl
@@ -165,6 +165,7 @@ udp_loop(#state{server = Host} = S) ->
       send_udp(Packet, S),
       udp_loop(S);
     stop ->
+      ?INFO_MSG("Stopping mod_grafite periodic_metrics..."),
       ok;
     _ ->
       udp_loop(S)
